9b5a790e3a7e6ff6b65d075b87869692c74faa24
lluanagabrieli <luanagabrieli4599@hotmail.com>
2024-06-13 20:50:38 -0300

    adiciona testes

--- __tests__/imc.test.ts
+++ __tests__/imc.test.ts
@@ -1,8 +1,27 @@
 import { imc } from "../src/imc";
 
 describe('Teste do IMC', () => {
-    test('Calculo do IMC com peso de 73 e altura de 1.80 deve retornar Peso Ideal', () => {
-        expect(imc.calculoImc(73, 1.80)).toBe('Peso ideal');
+    test('Calculo do IMC com peso de 50 e altura de 1.80 deve retornar baixo peso', () => {
+        expect(imc.calculoImc(50, 1.80)).toBe('Baixo peso');
+    });
+
+    test('Calculo do IMC com peso de 70 e altura de 1.70 deve retornar peso normal', () => {
+        expect(imc.calculoImc(70, 1.70)).toBe('Peso normal');
+    });
+
+    test('Calculo do IMC com peso de 70 e altura de 1.60 deve retornar sobrepeso', () => {
+        expect(imc.calculoImc(70, 1.60)).toBe('Sobrepeso');
+    });
+
+    test('Calculo do IMC com peso de 100 e altura de 1.75 deve retornar obesidade grau I', () => {
+        expect(imc.calculoImc(100, 1.75)).toBe('Obesidade grau I');
+    });
+
+    test('Calculo do IMC com peso de 100 e altura de 1.68 deve retornar obesidade grau II', () => {
+        expect(imc.calculoImc(100, 1.68)).toBe('Obesidade grau II');
+    });
+
+    test('Calculo do IMC com peso de 150 e altura de 1.65 deve retornar obesidade grau III', () => {
+        expect(imc.calculoImc(150, 1.65)).toBe('Obesidade grau III');
     });
 });
-
--- src/imc.ts
+++ src/imc.ts
@@ -1,10 +1,23 @@
 export class imc {
    static calculoImc(peso: number, altura: number): string {
         const total =  peso / (altura**2)
-        if(total >= 18.6 && total <= 24.9) {
-            return 'Peso ideal';
-        } else {
-            return 'Acima do peso'
+        if(total < 18) {
+            return 'Baixo peso';
+        } 
+        else if(total >= 18.5 && total <= 24.9) {
+            return 'Peso normal'
+        }
+        else if(total >= 25 && total <= 29.9) {
+            return 'Sobrepeso'
+        }
+        else if(total >= 30 && total <= 34.9) {
+            return 'Obesidade grau I'
+        }
+        else if(total >= 35 && total <= 39.9) {
+            return 'Obesidade grau II'
+        }
+        else {
+            return 'Obesidade grau III'
         }
     }
 }
